5ee8f5f557c80a0ae5a1995e59124db9ca68b4e5
Jonathan Trattner <accounts@jdtrat.com>
2021-02-13 15:32:16 -0500

    Added display_date function and improved the comment (:

--- reminders/R/remind_collaborator.R
+++ reminders/R/remind_collaborator.R
@@ -5,7 +5,15 @@ library(simplegit)
 my_repo <- Sys.getenv("MY_GITHUB_REPO")
 my_pat <- Sys.getenv("GITHUB_PAT")
 
+display_date <- function() {
+  # manually create a vector for date suffixes
+  date_suffixes <- c("st", "nd", "rd", rep("th", 17), "st", "nd", "rd", rep("th", 7), "st")
+  date <- format(Sys.Date(), "%d")
+  suffix <- date_suffixes[as.numeric(date)]
+  paste0(date, suffix)
+}
+
 gh_issue_comment(path = my_repo, 
                  issue_number = 1, 
-                 body = paste0("Hello, @smasongarrison! Happy ", format(Sys.Date(), "%A"), ". What a day today, the ", format(Sys.Date(), "%d"), ", is to review this submission 😄"),
+                 body = paste0("Hello, @smasongarrison! Happy ", format(Sys.Date(), "%A"), ". What a day today, the ", display_date(), " of ", format(Sys.Date(), "%B"), ", is to review this paper! 😄"),
                  .token = my_pat)
